b1fc67323f22fe644f4088a7b61b699920294d77
Dag <dagmnilsen@gmail.com>
2013-11-25 19:50:54 +0100

    dag

--- src/com/example/snms/utils/SnmsPrayTimeAdapter.java
+++ src/com/example/snms/utils/SnmsPrayTimeAdapter.java
@@ -15,6 +15,15 @@ import org.joda.time.LocalTime;
 import org.joda.time.MonthDay;
 import org.joda.time.chrono.GregorianChronology;
 
+import android.content.Context;
+import android.database.Cursor;
+import android.database.CursorIndexOutOfBoundsException;
+import android.widget.ImageView;
+
+import com.example.snms.AlarmUtilities;
+import com.example.snms.DBAdapter;
+import com.example.snms.PreyOverView;
+import com.example.snms.R;
 import com.example.snms.domain.PreyItem;
 import com.example.snms.domain.PreyItemList;
 
@@ -100,6 +109,72 @@ public class SnmsPrayTimeAdapter {
 		*/
 		
 	}
+	
+//public void checkAlarmStateAtStartup(PreyItem preyItem){
+//		
+//		Context context = PreyOverView.getAppContext();
+////		Intent intent = new Intent(context, AlarmReceiverActivity.class);
+//		  AlarmUtilities Util = new AlarmUtilities();
+////		  context.startActivity(myIntent);
+//		  
+//		//Get alarms after reboot
+//			DBAdapter db = new DBAdapter(context);
+//			db.open();
+//			
+//			Cursor cursor = db.getAllAlarms();
+//			
+//			try{
+//				cursor.moveToLast();
+//				do{
+//					
+//					
+//					Integer [] AlarmDate = Util.RefactorToIntegerFromDatabase(cursor.getString(1));
+//					
+//					DateTime dateTimeFromDB = new DateTime(AlarmDate[0], AlarmDate[1]+1, AlarmDate[2],
+//							AlarmDate[3], AlarmDate[4], AlarmDate[5]-1, 0); 
+//					String dateTimeFromDBString = dateTimeFromDB.getYear() + ":" + dateTimeFromDB.getMonthOfYear() + ":" +
+//							dateTimeFromDB.getDayOfMonth() + ":" + dateTimeFromDB.getHourOfDay() + ":" + 
+//							dateTimeFromDB.getMinuteOfHour() + ":" + dateTimeFromDB.getSecondOfMinute();
+//					
+//					System.out.println("dateTimeFromDB: " + dateTimeFromDBString);
+//					
+//					
+//						
+//						DateTime dateTimeFromEvent = preyItem.getTime();
+//						//Set correct alarm image at start-up
+//						String dateTimeFromEventString = dateTimeFromEvent.getYear() + ":" + 
+//						dateTimeFromEvent.getMonthOfYear() + ":" + dateTimeFromEvent.getDayOfMonth() + 
+//						":" + dateTimeFromEvent.getHourOfDay() + ":" + 
+//						dateTimeFromEvent.getMinuteOfHour() + ":" + dateTimeFromEvent.getSecondOfMinute();
+//						System.out.println("dateTimeFromEvent: " + dateTimeFromEventString);
+//						if(dateTimeFromEventString.equals(dateTimeFromDBString)==true){
+//							System.out.println("Dust4");
+//							
+//							ImageView image = (ImageView)v.findViewById(R.id.alarmclock_inactive);
+//							image.setImageResource(R.drawable.alarmclock);
+//							
+//							
+////							PreyItem clickedDetail = (PreyItem)l.getItemAtPosition(position);
+////							v.setAlarmBoolean(true);
+//							System.out.println("Yep");
+//						}
+//					
+//
+//					
+//				}while(cursor.moveToPrevious() && cursor.getInt(0)>0);
+//			
+//				System.out.println("AlarmPictures have been set on start-up");
+//				
+//			}catch(CursorIndexOutOfBoundsException e){
+//		    	System.out.println("There are no alarmPictures to be set...");
+//			
+//			db.close();
+//			
+//			
+//
+//			}	
+//	  
+//	}
 	
 
 }
